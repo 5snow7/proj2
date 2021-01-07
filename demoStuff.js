57e5f72a555ae0c67a01c8cd899c444ad8b578ab
mwomack <mwomack@pcoe.k12.ca.us>
2021-01-07 13:14:06 -0800

    aa

---
+++ demoStuff.js
@@ -0,0 +1,10 @@
+function arrayDemo(){
+	clearInterval(moveOne);
+	clear_Can();
+for(let k=1;k<6;k++){
+for(let j=1;j<10;j++){
+//for(let k=1;k<6;k++){
+tText.c.font="20px Ariel";
+tText.c.fillText("j"+j+" k"+k,50*j,20*k);
+}}
+}
